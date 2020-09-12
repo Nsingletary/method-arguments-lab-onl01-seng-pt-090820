d7f2c12be9abe732adcee349c7f60018d47a3ac3
candescent-tuple-8449 <singletarybush@gmail.com>
2020-09-12 16:40:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,12 +8,9 @@ introduction ("Josh")
 
 
 def introduction_with_language(name, language)
-<<<<<<< HEAD
-  puts "Hi, my name is #{name} and I am learning to program in #{language
-=======
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
->>>>>>> f9c324372e29e2790f97327b15e6bd13406efc07
 end
+
 
 introduction_with_language ("Dan", "Ember.js")
 # > Hi, my name is Dan and I am learning to program in Ember.js.
